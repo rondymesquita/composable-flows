a9296dd78fb69ec6a5924efe77df4fe17442bdaf
rondymesquita <rondinellimesquitas@gmail.com>
2021-10-23 16:46:23 -0300

    tests: test with mixed normal functions and promises

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -152,4 +152,36 @@ describe('Compose', () => {
 
     expect(callOrder).toEqual(['syncStageAlpha', 'syncStageBeta'])
   })
+
+  it('should call stages when stages are mixed normal functions and promises', async () => {
+    const callOrder: Array<string> = []
+    const syncStageAlpha = {
+      handle: jest.fn().mockImplementation(() => {
+        callOrder.push('syncStageAlpha')
+        return Promise.resolve()
+      }),
+    }
+
+    const syncStageBeta = {
+      handle: jest.fn().mockImplementation(() => {
+        callOrder.push('syncStageBeta')
+      }),
+    }
+
+    const param = 'email@email.com'
+
+    const sut = new Compose([syncStageAlpha.handle, syncStageBeta.handle])
+
+    expect(syncStageAlpha.handle).toBeCalledTimes(0)
+    expect(syncStageBeta.handle).toBeCalledTimes(0)
+    await sut.execute(param)
+
+    expect(syncStageAlpha.handle).toBeCalledTimes(1)
+    expect(syncStageAlpha.handle).toBeCalledWith(param)
+
+    expect(syncStageBeta.handle).toBeCalledTimes(1)
+    expect(syncStageBeta.handle).toBeCalledWith(param)
+
+    expect(callOrder).toEqual(['syncStageAlpha', 'syncStageBeta'])
+  })
 })
