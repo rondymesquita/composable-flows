c5ad2c4d0a51e77e68c4edc074d4abf0e3676b5e
rondymesquita <rondinellimesquitas@gmail.com>
2021-10-23 16:44:51 -0300

    tests: test with promises

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -16,6 +16,7 @@ describe('Compose', () => {
     const result = await sut.execute(param)
     expect(result).toEqual('alpha-result')
   })
+
   it('should call the stages in sequence when passing stage by push method', async () => {
     const callOrder: Array<string> = []
     const syncStageAlpha = {
@@ -117,5 +118,38 @@ describe('Compose', () => {
     expect(syncStageAlpha.handle).toBeCalledWith(param)
 
     expect(syncStageBeta.handle).not.toBeCalled()
+  })
+
+  it('should call stages when stages are promises', async () => {
+    const callOrder: Array<string> = []
+    const syncStageAlpha = {
+      handle: jest.fn().mockImplementation(() => {
+        callOrder.push('syncStageAlpha')
+        return Promise.resolve()
+      }),
+    }
+
+    const syncStageBeta = {
+      handle: jest.fn().mockImplementation(() => {
+        callOrder.push('syncStageBeta')
+        return Promise.resolve()
+      }),
+    }
+
+    const param = 'email@email.com'
+
+    const sut = new Compose([syncStageAlpha.handle, syncStageBeta.handle])
+
+    expect(syncStageAlpha.handle).toBeCalledTimes(0)
+    expect(syncStageBeta.handle).toBeCalledTimes(0)
+    await sut.execute(param)
+
+    expect(syncStageAlpha.handle).toBeCalledTimes(1)
+    expect(syncStageAlpha.handle).toBeCalledWith(param)
+
+    expect(syncStageBeta.handle).toBeCalledTimes(1)
+    expect(syncStageBeta.handle).toBeCalledWith(param)
+
+    expect(callOrder).toEqual(['syncStageAlpha', 'syncStageBeta'])
   })
 })
