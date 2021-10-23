8595c2e2bc75be9a6192e33d0c94c8d5a071c81b
rondymesquita <rondinellimesquitas@gmail.com>
2021-10-23 13:08:34 -0300

    feat: get result of last pipeline

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,86 @@
-import {hello} from './'
+import { Compose } from './'
 
-describe('index', () => {
-	it('test', () => {
-		expect(hello()).toEqual('world')
-	})
+describe('Compose', () => {
+  it('should instantiate', async () => {
+    expect(() => new Compose()).not.toThrow()
+  })
+  it('should call the stages in sequence when passing stage by push method', async () => {
+    const callOrder: Array<string> = []
+    const syncStageAlpha = {
+      handle: jest
+        .fn()
+        .mockImplementation(() => callOrder.push('syncStageAlpha')),
+    }
+
+    const syncStageBeta = {
+      handle: jest
+        .fn()
+        .mockImplementation(() => callOrder.push('syncStageBeta')),
+    }
+
+    const param = 'email@email.com'
+
+    const compose = new Compose()
+    compose.push(syncStageAlpha.handle)
+    compose.push(syncStageBeta.handle)
+
+    expect(syncStageAlpha.handle).toBeCalledTimes(0)
+    expect(syncStageBeta.handle).toBeCalledTimes(0)
+    await compose.execute(param)
+
+    expect(syncStageAlpha.handle).toBeCalledTimes(1)
+    expect(syncStageAlpha.handle).toBeCalledWith(param)
+
+    expect(syncStageBeta.handle).toBeCalledTimes(1)
+    expect(syncStageBeta.handle).toBeCalledWith(param)
+
+    expect(callOrder).toEqual(['syncStageAlpha', 'syncStageBeta'])
+  })
+
+  it('should call the stages in sequence when passing by constructor', async () => {
+    const callOrder: Array<string> = []
+    const syncStageAlpha = {
+      handle: jest
+        .fn()
+        .mockImplementation(() => callOrder.push('syncStageAlpha')),
+    }
+
+    const syncStageBeta = {
+      handle: jest
+        .fn()
+        .mockImplementation(() => callOrder.push('syncStageBeta')),
+    }
+
+    const param = 'email@email.com'
+
+    const compose = new Compose([syncStageAlpha.handle, syncStageBeta.handle])
+
+    expect(syncStageAlpha.handle).toBeCalledTimes(0)
+    expect(syncStageBeta.handle).toBeCalledTimes(0)
+    await compose.execute(param)
+
+    expect(syncStageAlpha.handle).toBeCalledTimes(1)
+    expect(syncStageAlpha.handle).toBeCalledWith(param)
+
+    expect(syncStageBeta.handle).toBeCalledTimes(1)
+    expect(syncStageBeta.handle).toBeCalledWith(param)
+
+    expect(callOrder).toEqual(['syncStageAlpha', 'syncStageBeta'])
+  })
+
+  it('should get the result of the last stage', async () => {
+    const syncStageAlpha = {
+      handle: jest.fn().mockReturnValue('alpha-result'),
+    }
+
+    const syncStageBeta = {
+      handle: jest.fn().mockReturnValue('beta-result'),
+    }
+
+    const param = 'email@email.com'
+
+    const compose = new Compose([syncStageAlpha.handle, syncStageBeta.handle])
+    const result = await compose.execute(param)
+    expect(result).toEqual('beta-result')
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-export const hello = () => {
-  return 'world'
+export class Compose {
+  private stages: Array<Function> = []
+
+  constructor(stages?: Array<Function>) {
+    if (stages) {
+      this.stages = stages
+    }
+  }
+
+  push(stage: Function) {
+    this.stages.push(stage)
+  }
+
+  async execute(param: any) {
+    let lastStageResult: any
+    for (let i = 0; i < this.stages.length; i++) {
+      const stage: Function = this.stages[i]
+      lastStageResult = (await stage(param)) as any
+    }
+    return lastStageResult
+  }
 }
