301885100eec74f1849ef637ac748452e97f5172
rondymesquita <rondinellimesquitas@gmail.com>
2021-10-24 20:49:05 -0300

    chore: remove dist folder from commit and added to gitignore

--- .dist/src/compose/compose.factory.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.makeComposeExecutor = void 0;
-const compose_executor_pipeline_1 = require("./compose-executor-pipeline");
-const makeComposeExecutor = (options, stageExecutor, stages) => {
-    return new compose_executor_pipeline_1.ComposeExecutorPipeline(stageExecutor, stages);
-};
-exports.makeComposeExecutor = makeComposeExecutor;
