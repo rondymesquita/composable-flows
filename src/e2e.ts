d1e17486c96f956b6e14bfe37eb20bab061d0a76
rondymesquita <rondinellimesquitas@gmail.com>
2021-10-24 18:21:02 -0300

    fix: wrong import

--- src/e2e.ts
+++ src/e2e.ts
@@ -2,29 +2,45 @@ import { ComposableFlow } from '.'
 
 class EmailValidator {
   validate(email: string): boolean {
-    console.log('>>validating email', email)
+    console.log('>> 1.1 validating email', email)
     return email === 'email@email.com'
   }
 }
 
 class EmailSender {
   async send(email: string, body: string): Promise<boolean> {
-    console.log('>>sending email', email, body)
+    console.log('>> 2.1. sending email', email, body)
 
     return new Promise((resolve) => {
       setTimeout(() => {
-        console.log('>>email sent')
+        console.log('>> 2.2. email sent')
         resolve(true)
-      }, 1500)
+      }, 700)
+    })
+  }
+}
+
+class Notifier {
+  async notify(): Promise<string> {
+    console.log('>> 3.1. sending notification')
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log('>> 3.2. notification sent')
+        resolve('notification sent')
+      }, 300)
     })
   }
 }
 
 const emailSender = new EmailSender()
 const emailValidator = new EmailValidator()
+const notifier = new Notifier()
+
 const flow = new ComposableFlow([
   () => emailValidator.validate('email@email.com'),
   () => emailSender.send('email@email.com', '# hello'),
+  () => notifier.notify(),
 ])
 
 flow.execute().then((response) => {
