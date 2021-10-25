446d635adf3761cef0a2bc7ad446ab028a684590
rondymesquita <rondinellimesquitas@gmail.com>
2021-10-24 21:29:55 -0300

    chore: add example for stop on error disabled

---
+++ examples/continue-on-error-options.ts
@@ -0,0 +1,32 @@
+import { ComposableFlow } from '../src'
+
+export class GetUserInfo {
+  get(email: string, role: string): any {
+    console.log('This stage fails')
+    throw new Error('Error when getting user information')
+  }
+}
+
+export class EmailSender {
+  send(email: string, body: string): any {
+    console.log('This will be executed even if previous stage fails')
+  }
+}
+
+const getUserInfo = new GetUserInfo()
+const emailSender = new EmailSender()
+
+const options = {
+  stopOnError: false,
+}
+const flow = new ComposableFlow(
+  [
+    () => getUserInfo.get('email@email.com', 'admin'),
+    () => emailSender.send('email@email.com', '#hello'),
+  ],
+  options,
+)
+
+flow.execute().then((lastResult) => {
+  console.log('done', lastResult)
+})
