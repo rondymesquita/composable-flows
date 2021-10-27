2c08b7b70d40857e6646b9f4fe63c9427e58c945
rondymesquita <rondinellimesquitas@gmail.com>
2021-10-26 21:25:04 -0300

    chore: adjust small changes on examples

--- examples/pipeline-mode.ts
+++ examples/pipeline-mode.ts
@@ -52,7 +52,6 @@ const flow = new ComposableFlow(
     () => {
       return getUserInfo.get('email@email.com', 'admin')
     },
-    // getUserInfo.get,
     emailSender.send,
     database.storeLog,
   ],
@@ -60,5 +59,5 @@ const flow = new ComposableFlow(
 )
 
 flow.execute().then((lastResult) => {
-  // console.log('done', lastResult)
+  console.log('done', lastResult)
 })
